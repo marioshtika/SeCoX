6e5e172276d8fc24e9bf313bfecf0759fd165db2
Mario Shtika <marioshtika@gmail.com>
2014-08-20 20:54:14 +0300

    fixed bug on calculating Precision & Recall

--- precision-recall.php
+++ precision-recall.php
@@ -18,26 +18,37 @@
 		echo '<th>Recall</th>';
 		echo '</tr>';
 		while($row_rankings = $result->fetch_assoc()) {
-			echo '<tr>';
-			echo '<td>'.$row_rankings['title'].'</td>';
-
-			echo '<td>';
+			// found answers
 			$query_links_count = "SELECT COUNT(*) as all_links_row FROM parsingrows WHERE site = '".$row_rankings['title']."' AND `dbpedia-uri` <> ''";
 			$result_links_count = $mysqli->query($query_links_count) or die($mysqli->error.__LINE__);
 			$row_links_count = $result_links_count->fetch_assoc();
-			
+			$all_answers = $row_links_count['all_links_row'];
+			// correct answers
 			$query_matches_count = "SELECT COUNT(*) as all_matches_row FROM parsingrows WHERE site = '".$row_rankings['title']."' AND `dbpedia-score` > ".constant("DBPEDIA_SCORE")." AND `oliver-score` > ".constant("OLIVER_SCORE")." AND `levenshtein-score` < ".constant("LEVENSHTEIN_SCORE")." AND	`dbpedia-uri` <> ''";
 			$result_matches_count = $mysqli->query($query_matches_count) or die($mysqli->error.__LINE__);
 			$row_matches_count = $result_matches_count->fetch_assoc();
-			
-			echo ($row_links_count['all_links_row'] != 0) ? $row_matches_count['all_matches_row'].' / '.$row_links_count['all_links_row'].' = '.$row_matches_count['all_matches_row']/$row_links_count['all_links_row'] : '0';
-			echo '</td>';
-			echo '<td>';
+			$correct_answers = $row_matches_count['all_matches_row'];
+			// all links
 			$query_university_count = "SELECT COUNT(*) as all_university_row FROM parsingrows WHERE site = '".$row_rankings['title']."'";
 			$result_university_count = $mysqli->query($query_university_count) or die($mysqli->error.__LINE__);
 			$row_university_count = $result_university_count->fetch_assoc();
-			echo ($row_university_count['all_university_row'] != 0) ? $row_matches_count['all_matches_row'].' / '.$row_university_count['all_university_row'].' = '.$row_matches_count['all_matches_row']/$row_university_count['all_university_row'] : '0';
+			$all_links = $row_university_count['all_university_row'];
+			// incorrect answers
+			$incorrect_answers = $all_answers - $correct_answers;
+			$not_found_answers = $all_links - $all_answers;
+			
+			echo '<tr>';
+			
+			echo '<td>'.$row_rankings['title'].'</td>';
+
+			echo '<td>';
+			echo ($correct_answers + $incorrect_answers != 0) ? $correct_answers.' / '.($correct_answers + $incorrect_answers).' = '.$correct_answers/($correct_answers + $incorrect_answers) : '0';
 			echo '</td>';
+			
+			echo '<td>';
+			echo ($correct_answers + $not_found_answers != 0) ? $correct_answers.' / '.($correct_answers + $not_found_answers).' = '.$correct_answers/($correct_answers + $not_found_answers) : '0';
+			echo '</td>';
+			
 			echo '</tr>';
 		}
 	} else {
